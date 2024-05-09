f94e33a8d5076256b7bfc6b99a6420cc16454de4
GayathriThanishka <gayathrithanishka27@gmail.com>
2024-05-09 15:47:37 +0530

    feat - aligments

--- lib/DesktopTask/login.dart
+++ lib/DesktopTask/login.dart
@@ -346,7 +346,7 @@ class _DesktopLoginState extends State<DesktopLogin> {
                         child: Divider(
                           endIndent: 20,
                           color: Colors.black,
-                          height: 35,
+                          height: 36,
 
                         ),
                       ),
